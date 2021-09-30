64996820bb178cac0b7bb71092c872d542824a28
root <NicholasJWallace@protonmail.com>
2021-09-30 15:27:23 -0700

    >5

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+\#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 
@@ -12,4 +12,3 @@ elif lastdigit == 0:
 else:
         print("Last digit of {} is {} and is less than 6 and not 0".format(
                     number, lastdigit))
-        
