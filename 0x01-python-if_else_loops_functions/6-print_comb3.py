3f65cf1cd56075fd0f5a886aa0e4362dc1b0213e
Niqualis <NicholasJWallace@protonmail.com>
2021-10-07 20:43:12 -0500

    possible-2-combos

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(10):
-        if y > x:
-            if (x != 8 or y != 9):
-                print("{}{}".format(x, y), end=', ')
-            else:
-                print("{}{}".format(x, y))
+for i in range(10):
+    for n in range(10):
+        if i < n:
+            print("{}{}".format(i, n), end="\n" if i == 8 and n == 9 else ", ")
