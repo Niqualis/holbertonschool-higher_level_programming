9c5ba8e9aa3105dc1aa2a6706cbaa6cf4b3a51e8
Niqualis <NicholasJWallace@protonmail.com>
2021-10-12 22:24:42 -0500

    add-2-integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""This module adds two integers or floats"""
+
+
 def add_integer(a, b=98):
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-    if type(a) != int:
+    """Adds two integers or floats(which are then cast as ints)"""
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int:
+
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    else:
-        return a + b
+
+    return int(a) + int(b)
