cd797b10a4363667d4caabb5d87589210bd85193
Niqualis <NicholasJWallace@protonmail.com>
2021-10-22 09:34:01 -0500

    add-args-save-2-a-file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """adds all arguments to a list and saves to a file"""
 
+
 import sys
 import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
