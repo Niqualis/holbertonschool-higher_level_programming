e9a0cc3bb3090b54d01587925eba2b45c46bad42
Niqualis <NicholasJWallace@protonmail.com>
2021-11-02 20:24:10 -0500

    all-records

--- 0x00-SQL_introduction/16-no_link.sql
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- a script that lists all records of the table
-SELECT SCORE, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT SCORE, name FROM second_table
+ WHERE name IS NOT NULL
+ ORDER BY score DESC;
