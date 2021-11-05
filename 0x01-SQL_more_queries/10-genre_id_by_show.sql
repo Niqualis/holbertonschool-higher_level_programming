e23d1a2e36563d2054b92023dd7c55123b4659df
Niqualis <NicholasJWallace@protonmail.com>
2021-11-05 14:25:46 -0500

    tv-shows

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Import database dump to mysql
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id = 1;
+JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
