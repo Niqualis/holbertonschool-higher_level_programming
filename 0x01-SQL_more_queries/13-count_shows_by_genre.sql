6510d4f828e6d59bed804a5410e49434193e44c0
Niqualis <NicholasJWallace@protonmail.com>
2021-11-05 15:52:22 -0500

    count-by-genre

---
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- A script that lists all genres, and displays number of shows
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id = genre_id
+GROUP BY genre_id ORDER BY number_of_shows DESC;
