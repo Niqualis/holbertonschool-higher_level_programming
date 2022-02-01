6f39233b31b292943030f7d02ac42990ca425cdf
Niqualis <83564728+Niqualis@users.noreply.github.com>
2022-02-01 13:34:11 -0600

    Delete 0x00-SQL_introduction directory

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++
@@ -1,2 +0,0 @@
---  a script that creates the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE if not EXISTS hbtn_0c_0;
--- 0x00-SQL_introduction/7-insert_value.sql
+++
@@ -1,2 +0,0 @@
--- a script that inserts a new row in the table
-INSERT INTO first_table VALUES('89', 'Holberton School');
