9c6af9b9cafd41265f54b51fd6b606c859471145
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 15:56:30 -0600

    task 7

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- create DB hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT unique AUTO AUTO_INCREMENT NOT NULL PRIMARY KEY,
+state_id INT NOT NULL, FOREIGN KEY(state id) REFERENCES hbtn_0d_usa.states(id),
+name VARCHAR(256) NOT NULL);
