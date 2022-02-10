ee8d08b959abe7f0e8021e72bce1b9859f2bbf1d
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 14:26:59 -0600

    task 0

--- 0x01-SQL_more_queries/0-privileges.sql
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- script that lists all priveleges of user_0d_1/user_0d_2
-SHOW GRANT FOR 'user_0d_1'@'localhost';'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
