b824b985e12deeacf6e4103e3b2bdb124f7455cb
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 14:39:36 -0600

    task 1

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user if not exist, and allow privelege
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
