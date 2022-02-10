b5fb20132d93088b9ac4d45c4b76a6138d463b79
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 15:28:04 -0600

    task 6

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- create DB hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT unique  NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
