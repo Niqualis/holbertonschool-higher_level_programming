ec12368e5b387726c8dba615d46ace13de6af8f2
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 16:15:53 -0600

    task 8

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- list all cities of Cali found in the DB
-SELECT id, NAME FROM cities WHERE state_id = (select id FROM states WHERE name = 'California')
+SELECT id, NAME FROM cities WHERE state_id = (select id FROM states WHERE name = 'california')
 ORDER BY id;
