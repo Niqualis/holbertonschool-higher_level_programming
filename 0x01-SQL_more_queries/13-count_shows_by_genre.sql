fda764dcf6bfd6cacafbe43b552be379b3c8bcc8
Niqualis <NicholasJWallace@protonmail.com>
2022-02-10 18:40:29 -0600

    task 13

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- import DB dump from hbtn_0d_tvshows
 -- list genres and display number of shows linked
-SELECT tv_show_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_show_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres LEFT JOIN tv_genres
 ON tv_genres.id = genre_id
-GROUP BY genre_id ORDER BY number_of_shows DESC;
+GROUP BY genre_id ORDER BY count(*) DESC;
