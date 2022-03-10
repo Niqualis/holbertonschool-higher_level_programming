8ee2ff1d97f48890d60a6127fe5d5c0d54a2f7e2
Niqualis <NicholasJWallace@protonmail.com>
2022-03-10 15:22:57 -0600

    fetch

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """fetches URL of https://intranet.hbtn.io/status"""
-
 if __name__ == '__main__':
     import urllib.request
+    import urllib.parse
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        html = reply.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(str(html)))
-        HTMLdecode = html.decode('UTF-8')
-        print('\t- utf8 content: {}'.format(HTMLdecode))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
+        
