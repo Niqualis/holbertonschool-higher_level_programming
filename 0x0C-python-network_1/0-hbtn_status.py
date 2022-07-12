7dcf2ca5301491fb3bc53b68167905a74672d4d9
Niqualis <NicholasJWallace@protonmail.com>
2022-07-12 16:03:27 -0500

    permissions

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""fetches URL of https://intranet.hbtn.io/status"""
-
+'''This module fetches a URL with urllib'''
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body respons:')
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
         print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('UTF-8')))
