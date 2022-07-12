7f2257e48dfe1f0553b2a380b3ff79c0913c4890
Niqualis <NicholasJWallace@protonmail.com>
2022-07-12 16:02:26 -0500

    permissions

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """fetches URL of https://intranet.hbtn.io/status"""
 
+
+
 if __name__ == '__main__':
     import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(str(html)))
-        HTMLdecode = html.decode('UTF-8')
-        print('\t- utf8 content: {}'.format(HTMLdecode))
+        print('Body respons:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('UTF-8')))
