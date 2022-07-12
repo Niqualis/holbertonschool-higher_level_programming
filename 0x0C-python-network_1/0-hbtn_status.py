2f2184fbf9cd8b63f4ea301196c30a74a8861b2b
Niqualis <NicholasJWallace@protonmail.com>
2022-07-12 15:54:21 -0500

    permissions

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 
 if __name__ == '__main__':
     import urllib.request
-    import urllib.parse
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
-        html = reply.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+        print("\t- content: {}".format(str(html)))
+        HTMLdecode = html.decode('UTF-8')
+        print('\t- utf8 content: {}'.format(HTMLdecode))
